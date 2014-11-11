1d3ad7cae5e9036aba5e6ac266f3cae885079e9e
David Silver <David.L.Silver@gmail.com>
2014-11-11 15:31:54 +1030

    Have nautilus show Desktop files.

---
+++ setup.d/desktop.sh
@@ -0,0 +1,4 @@
+gsettings set org.gnome.desktop.background show-desktop-icons true
+gsettings set org.gnome.nautilus.desktop volumes-visible true
+gsettings set org.gnome.nautilus.desktop netowrk-icon-visible true
+
