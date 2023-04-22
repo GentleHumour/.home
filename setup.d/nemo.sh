a5cbee154dcbea7e0493188a4f55e7f1941764d9
David <David.L.Silver@gmail.com>
2023-04-23 01:08:34 +0930

    Configure Nemo as default file handler.

---
+++ setup.d/nemo.sh
@@ -0,0 +1,2 @@
+# https://askubuntu.com/questions/1066752/how-to-set-nemo-as-the-default-file-manager-in-ubuntu#1173861
+xdg-mime default nemo.desktop inode/directory application/x-gnome-saved-search
