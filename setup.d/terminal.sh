5e9c5380a969936057327e082827796617c584be
David Silver <David.L.Silver@gmail.com>
2023-04-25 19:31:40 +0930

    Use `terminator` as terminal in `nemo` file manager.

---
+++ setup.d/terminal.sh
@@ -0,0 +1,3 @@
+gsettings set org.gnome.desktop.default-applications.terminal exec 'terminator'
+gsettings set org.cinnamon.desktop.default-applications.terminal exec 'terminator'
+gsettings set org.gnome.gthumb.terminal command 'terminator --window --working-directory=%f'
